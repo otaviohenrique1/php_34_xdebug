7518f073979ed64941ec07dc69013c8a2f455288
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2024-05-10 15:23:32 -0300

    commit 2

--- alura-xdebug/teste.php
+++ alura-xdebug/teste.php
@@ -4,4 +4,9 @@ require 'outro.php';
 $variavel = 115315414564348631405346546543421654125;
 var_dump($variavel);
 var_dump('teste');
-funcao1("Um parâmetro");
+
+try {
+  funcao1("Um parâmetro");
+} catch (\Throwable $th) {
+  echo $th->getMessage();
+}
